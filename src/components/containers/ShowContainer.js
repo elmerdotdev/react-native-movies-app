7ae5b1fbbfa1569dfcafce0166cb5a82f7fa3f98
Elmer Balbin Jr <contact@elmer.dev>
2023-02-03 01:38:38 -0800

    show screen

--- src/components/containers/ShowContainer.js
+++ src/components/containers/ShowContainer.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { Center, Text, View, VStack } from "native-base"
+import { Center, Heading, Text, View, VStack, Image } from "native-base"
 import { getMovie, getTvShow, getPerson } from "../services/api"
 
 const ShowContainer = ({ navigation, route }) => {
@@ -7,21 +7,22 @@ const ShowContainer = ({ navigation, route }) => {
   const [data, setData] = useState({})
 
   if (mediaType === 'movie') {
-      getMovie(showId).then(result => setData(result))
+    getMovie(showId).then(result => setData(result))
   } else if (mediaType === 'tv') {
-      getTvShow(showId).then(result => setData(result))
+    getTvShow(showId).then(result => setData(result))
   } else if (mediaType === 'person') {
     getPerson(showId).then(result => setData(result))
   }
 
-  useEffect(() => {
-    console.log(data)
-  }, [data])
-
   return (
     <Center>
-      <VStack>
-
+      <VStack width="100%" p="10">
+        <Heading mb={5} textAlign="center">{data.title || data.name}</Heading>
+        {data.poster_path && <Image source={{
+          uri: `https://image.tmdb.org/t/p/w500/${data.poster_path}`
+        }} alt="Alternate text" size="2xl" margin="auto" />}
+        <Text my={5}>{data.overview}</Text>
+        <Text fontSize="xs">Popularity: {data.popularity} | Release Date: {data.release_date}</Text>
       </VStack>
     </Center>
   )
