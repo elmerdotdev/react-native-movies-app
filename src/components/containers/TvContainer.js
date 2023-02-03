00154a024d9c9ac603f301f7b580d144b0e4003c
Elmer Balbin Jr <contact@elmer.dev>
2023-02-02 23:22:46 -0800

    added loading

--- src/components/containers/TvContainer.js
+++ src/components/containers/TvContainer.js
@@ -3,15 +3,19 @@ import { VStack } from "native-base"
 import { getTvShows } from "../services/api"
 import TvShowsForm from '../forms/TvShowsForm'
 import ShowList from '../lists/ShowList'
+import Loading from '../layout/Loading'
 
 const TvContainer = ({ navigation }) => {
   const [tvShows, setTvShows] = useState([])
   const [selection, setSelection] = useState('airing_today')
+  const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
+    setIsLoading(true)
     getTvShows(selection).then(
       data => {
         setTvShows(data)
+        setIsLoading(false)
       },
       error => {
         throw error
@@ -26,7 +30,7 @@ const TvContainer = ({ navigation }) => {
   return (
     <VStack width="100%" mb={10} pb={10}>
       <TvShowsForm updateSelection={updateSelection} />
-      <ShowList shows={tvShows} navigation={navigation} mediaType='tv' />
+      {isLoading ? <Loading /> : <ShowList shows={tvShows} navigation={navigation} mediaType='tv' />}
     </VStack>
   )
 }
