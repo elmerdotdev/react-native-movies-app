28159051fdaf11c8fd430a6c35338c0f56379a50
Elmer Balbin Jr <contact@elmer.dev>
2023-02-03 10:13:37 -0800

    removed useEffect

--- src/components/containers/ShowContainer.js
+++ src/components/containers/ShowContainer.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { Center, Heading, Text, View, VStack, Image, ScrollView } from "native-base"
 import { getMovie, getTvShow, getPerson } from "../services/api"
 
